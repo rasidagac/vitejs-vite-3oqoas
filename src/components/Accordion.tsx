f5aa2dba7c940f3c282654e4e24c3d25f745a3d5
rasidagac <rasitagac@hotmail.com>
2023-01-23 15:32:21 +0300

    1 file modified, 1 added

---
+++ src/components/Accordion.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+interface Props {
+  items: Array<{ id: string; label: string; content: string }>;
+}
+
+function Accordion({ items }: Props) {
+  const [expandedIndex, setExpandedIndex] = useState(0);
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
+    return (
+      <div key={item.id}>
+        <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+        {isExpanded && <div>{item.content}</div>}
+      </div>
+    );
+  });
+
+  return <div>{renderedItems}</div>;
+}
+
+export default Accordion;
